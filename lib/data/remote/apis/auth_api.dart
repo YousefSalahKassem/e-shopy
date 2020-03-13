07c534d5840183cf413d286a79bf3bf0d1027a17
Ahmed Abu Eldahab <dahabit@gmail.com>
2020-03-13 10:25:10 +0200

    more refactoring

--- lib/data/remote/apis/auth_api.dart
+++ lib/data/remote/apis/auth_api.dart
@@ -1,5 +1,4 @@
-import 'package:kortobaa_flutter_provider_boilerplate/bloc/value_objects/login_request_body.dart';
-
+import 'package:kortobaa_flutter_provider_boilerplate/bloc/vaildators/login_request_body.dart';
 import 'package:kortobaa_flutter_provider_boilerplate/bloc/models/user.dart';
 import 'package:kortobaa_flutter_provider_boilerplate/data/remote/dio_client.dart';
 import 'package:kortobaa_flutter_provider_boilerplate/services/exceptions/fetch_exception.dart';
--- lib/services/interfaces/i_auth_api.dart
+++ lib/services/interfaces/i_auth_api.dart
@@ -1,5 +1,5 @@
 import 'package:kortobaa_flutter_provider_boilerplate/bloc/models/user.dart';
-import 'package:kortobaa_flutter_provider_boilerplate/bloc/value_objects/login_request_body.dart';
+import 'package:kortobaa_flutter_provider_boilerplate/bloc/vaildators/login_request_body.dart';
 
 abstract class IAuthApi {
   Future<User> loginUser(LoginRequestBody loginRequestBody);
