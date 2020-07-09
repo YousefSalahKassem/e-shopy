856baaea9ed299cd6acb84350d1877b0b3a7a4ce
Ayman-Barghout <ayman.a.barghout@gmail.com>
2020-07-09 15:35:55 +0200

    Added check for app update for Android and iOS (it might break the app if installed in release mode from another source than google play)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:provider_boilerplate/helpers/locale_preferences.dart';
 import 'package:provider_boilerplate/themes/theme.dart';
 import 'package:provider_boilerplate/routes/router.gr.dart';
@@ -9,6 +12,13 @@ import 'package:logging/logging.dart' as log;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid && kReleaseMode) {
+    final AppUpdateInfo info = await InAppUpdate.checkForUpdate();
+    if (info.updateAvailable) {
+      await InAppUpdate.performImmediateUpdate();
+    }
+  }
   // Log everything in debug builds.  Log warnings (and up) in release builds.
   log.Logger.root.level = kDebugMode ? log.Level.ALL : log.Level.WARNING;
   log.Logger.root.onRecord.listen((log.LogRecord rec) {
--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:provider_boilerplate/helpers/localized_upgrader_messages.dart';
+import 'package:upgrader/upgrader.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -9,6 +11,13 @@ class HomeScreen extends StatelessWidget {
         title: Text(translate('greeting')),
         centerTitle: true,
       ),
+      body: UpgradeAlert(
+          showIgnore: false,
+          showLater: false,
+          messages: LocalizedMessages(
+              code:
+                  LocalizedApp.of(context).delegate.currentLocale.languageCode),
+          child: const Center(child: Text('Home screen'))),
     );
   }
 }
