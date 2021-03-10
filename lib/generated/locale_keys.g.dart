87ecd2618f6a8403f2a188284ed24bfb9a2d5fb4
kortobaaEnigma <mohamedsalama@kortobaa.com>
2021-03-11 01:40:56 +0200

    Apply Generated LocaleKeys in easy_localization package

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,13 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+// ignore_for_file: constant_identifier_names
+abstract class LocaleKeys {
+  static const appName = 'appName';
+  static const greeting = 'greeting';
+  static const home_screen_body = 'home_screen.body';
+
+  static const home_screen_toggle_language = 'home_screen.toggle_language';
+  static const home_screen = 'home_screen';
+  static const ar = 'ar';
+  static const en = 'en';
+  static const start = 'start';
+}
--- lib/ui/screens/home_screen/home_screen.dart
+++ lib/ui/screens/home_screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_boilerplate/helpers/ui/ui_helpers.dart';
-
+import 'package:flutter_boilerplate/generated/locale_keys.g.dart';
 import 'package:upgrader/upgrader.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,7 +9,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(tr("greeting")),
+        title: Text(
+            tr(LocaleKeys.greeting)), // <-- Generated LocaleKeys usage example
         centerTitle: true,
       ),
       body: UpgradeAlert(
