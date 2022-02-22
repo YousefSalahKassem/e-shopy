818a8a4c591ff089fdd46c2bc459ff53fb005073
Moustafa Essam Ibrahim <100077177+moustafa-essam@users.noreply.github.com>
2022-02-22 11:51:32 +0200

    Update initialization_exception.dart

--- lib/exceptions/initialization_exception.dart
+++ lib/exceptions/initialization_exception.dart
@@ -1,7 +1,13 @@
-class UninitializedException extends Error {
-  UninitializedException(this._fieldName, [this._type]);
+class UninitializedException implements Exception {
+  const UninitializedException(this._fieldName, [this._type]);
   final String _fieldName;
   final Type? _type;
+
   String get message =>
       '$_fieldName${_type != null ? " of type: $_type" : ""} was not initialized';
+
+  @override
+  String toString() {
+    return "UninitializedException: $message";
+  }
 }
