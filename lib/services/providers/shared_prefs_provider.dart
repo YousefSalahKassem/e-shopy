bd73f8d90231f7ed874418f76e861b7040fcc05f
Moustafa Essam Ibrahim <100077177+moustafa-essam@users.noreply.github.com>
2022-02-22 11:54:09 +0200

    Update shared_prefs_provider.dart

--- lib/services/providers/shared_prefs_provider.dart
+++ lib/services/providers/shared_prefs_provider.dart
@@ -3,5 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final sharedPrefsProvider = Provider<SharedPreferences>(
-  (ref) => throw UninitializedException("sharedPrefsProvider"),
+  (ref) => throw const UninitializedException("sharedPrefsProvider", SharedPreferences),
 );
