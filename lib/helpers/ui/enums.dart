27793b55a2f6cc1d3994df424d4e8c4c902da9de
Mohamed Gamal <mohmedgamalma3011@gmail.com>
2022-08-08 21:53:17 +0200

    add theme docs string

--- lib/helpers/ui/enums.dart
+++ lib/helpers/ui/enums.dart
@@ -1,2 +1,7 @@
-enum ThemeFlavor { light, dark }
+import 'package:flutter_boilerplate/themes/flavors/app_theme_flavor.dart';
 
+///### [ThemeFlavor] enum to hold the flavors names
+///
+/// you can add your custom flavor by add the flavor name in [ThemeFlavor]
+/// and add the use case for this flavor in [AppThemeFlavor]
+enum ThemeFlavor { light, dark }
--- lib/themes/app_theme_provider.dart
+++ lib/themes/app_theme_provider.dart
@@ -5,16 +5,26 @@ import 'package:flutter_boilerplate/services/providers/shared_prefs_provider.dar
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// ### [AppTheme] provider hold the current theme flavor
+///
+/// use [AppTheme.provider] in riverpod to watch the provider and
+/// get current theme flavor,default flavor is the first flavor in [ThemeFlavor]
+///
+/// change the flavor by call [setThemeFlavor].
+///
+/// [AppTheme] use [SharedPreferences] to cache the current theme flavor,
+/// [kSavedThemeStringKey] is key used to save the flavor,
+/// saved value is the flavor name.
 class AppTheme extends ChangeNotifier {
   static final provider = ChangeNotifierProvider<AppTheme>(
-    (ref) => AppTheme(ref.read(AppSharedPreferences.provider)),
+    (ref) => AppTheme(ref.watch(AppSharedPreferences.provider)),
   );
 
   //* Dependency
   late final SharedPreferences _sharedPreferences;
 
   //* State
-  ThemeFlavor _themeFlavor = ThemeFlavor.light;
+  ThemeFlavor _themeFlavor = ThemeFlavor.values.first;
 
   //* Getters
   ThemeFlavor get themeFlavor => _themeFlavor;
@@ -24,16 +34,18 @@ class AppTheme extends ChangeNotifier {
   // this test
   AppTheme(this._sharedPreferences) {
     // Load Theme as soon as the provider is created
-    load();
+    ensureInitialized();
   }
-
+  /// ### change current flavor.
+  ///
+  /// use notifyListeners to update any widget watch [AppTheme] provider.
   Future<void> setThemeFlavor(ThemeFlavor flavor) async {
     _themeFlavor = flavor;
     await _sharedPreferences.setString(kSavedThemeStringKey, flavor.name);
     notifyListeners();
   }
 
-  void load() {
+  void ensureInitialized() {
     final String? savedFlavor =
         _sharedPreferences.getString(kSavedThemeStringKey);
     if (savedFlavor != null) {
